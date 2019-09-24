bf6a93251d122c3a3125b9e2eb1f90f29380d04f
pushpraj789 <55019385+pushpraj789@users.noreply.github.com>
2019-09-24 15:52:28 +0530

    Add files via upload

---
+++ Inventory.Contracts/BLContracts/ISystemUserBL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Capgemini.Inventory.Entities;
+
+namespace Capgemini.Inventory.Contracts.BLContracts
+{
+    public interface ISystemUserBL : IDisposable
+    {
+        Task<bool> AddSystemUserBL(SystemUser newSystemUser);
+        Task<List<SystemUser>> GetAllSystemUsersBL();
+        Task<SystemUser> GetSystemUserBySystemUserIDBL(Guid searchSystemUserID);
+        Task<List<SystemUser>> GetSystemUsersByNameBL(string supplierName);
+        Task<SystemUser> GetSystemUserByEmailBL(string email);
+        Task<SystemUser> GetSystemUserByEmailAndPasswordBL(string email, string password);
+        Task<bool> UpdateSystemUserBL(SystemUser updateSystemUser);
+        Task<bool> UpdateSystemUserPasswordBL(SystemUser updateSystemUser);
+        Task<bool> DeleteSystemUserBL(Guid deleteSystemUserID);
+    }
+}
+
+
---
+++ Inventory.Contracts/IDistributorAddressBL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Capgemini.Inventory.Entities;
+
+namespace Capgemini.Inventory.Contracts.BLContracts
+{
+    public interface IDistributorAddressBL : IDisposable
+    {
+        Task<bool> AddDistributorAddressBL(DistributorAddress newDistributorAddress);
+        Task<List<DistributorAddress>> GetAllDistributorAddressesBL();
+        Task<DistributorAddress> GetDistributorAddressByDistributorAddressIDBL(Guid searchAddressID);
+        Task<bool> UpdateDistributorAddressBL(DistributorAddress updateDistributorAddress);
+        Task<bool> DeleteDistributorAddressBL(Guid deleteAddressID);
+    }
+}
