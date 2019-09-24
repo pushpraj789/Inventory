36dd65dcfce93ab23c1254656218c06ce64e8140
pushpraj789 <55019385+pushpraj789@users.noreply.github.com>
2019-09-24 15:50:19 +0530

    Add files via upload

---
+++ Inventory.Entities/DistributorAddress.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Capgemini.Inventory.Helpers.ValidationAttributes;
+
+namespace Capgemini.Inventory.Entities
+{
+    /// <summary>
+    /// Interface for SystemUser Entity
+    /// </summary>
+    public interface IDistributorAddress
+    {
+        Guid DistributorAddressID { get; set; }
+        Guid DistributorID { get; set; }
+        string AddressLine1 { get; set; }
+        string AddressLine2 { get; set; }
+        string PinCode { get; set; }
+        string City { get; set; }
+        string State { get; set; }
+    }
+
+
+    /// <summary>
+    /// Represents SystemUser
+    /// </summary>
+    public class DistributorAddress : IDistributorAddress
+    {
+        /* Auto-Implemented Properties */
+        [Required("Distributor Address ID can't be blank.")]
+        public Guid DistributorAddressID { get; set; }
+
+        
+        [Required("AddressLine1 should be supplier or  distributor")]
+        public string AddressLine1 { get; set; }
+
+        [Required("AddressLine2 should be supplier or  distributor")]
+        public string AddressLine2 { get; set; }
+
+        [Required("PinCode should not be blank")]
+        public string PinCode { get; set; }
+
+        [Required("City should not be blank")]
+        public string City { get; set; }
+
+        [Required("State should not be blank")]
+        public string State { get; set; }
+
+        [Required("Distributor  ID can't be blank.")]
+        public Guid DistributorID { get; set; }
+
+
+        public DistributorAddress()
+        {
+            DistributorAddressID = default(Guid);
+            DistributorID = default(Guid);
+            AddressLine1 = string.Empty;
+            AddressLine2 = string.Empty;
+            PinCode = string.Empty;
+            City = string.Empty;
+            State = string.Empty;
+            
+        }
+
+    }
+}
---
+++ Inventory.Entities/SystemUser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Capgemini.Inventory.Helpers.ValidationAttributes;
+
+namespace Capgemini.Inventory.Entities
+{
+    /// <summary>
+    /// Interface for SystemUser Entity
+    /// </summary>
+    public interface ISystemUser
+    {
+        Guid SystemUserID { get; set; }
+        string SystemUserName { get; set; }
+        string Email { get; set; }
+        string Password { get; set; }
+        DateTime CreationDateTime { get; set; }
+        DateTime LastModifiedDateTime { get; set; }
+    }
+
+    /// <summary>
+    /// Represents SystemUser
+    /// </summary>
+    public class SystemUser : ISystemUser, IUser
+    {
+        /* Auto-Implemented Properties */
+        [Required("SystemUser ID can't be blank.")]
+        public Guid SystemUserID { get; set; }
+
+        [Required("SystemUser Name can't be blank.")]
+        [RegExp(@"^(\w{2,40})$", "SystemUser Name should contain only 2 to 40 characters.")]
+        public string SystemUserName { get; set; }
+
+        [Required("Email can't be blank.")]
+        [RegExp(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", "Email is invalid.")]
+        public string Email { get; set; }
+
+        [Required("Password can't be blank.")]
+        [RegExp(@"((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15})", "Password should be 6 to 15 characters with at least one digit, one uppercase letter, one lower case letter.")]
+        public string Password { get; set; }
+
+        public DateTime CreationDateTime { get; set; }
+        public DateTime LastModifiedDateTime { get; set; }
+
+        /* Constructor */
+        public SystemUser()
+        {
+            SystemUserID = default(Guid);
+            SystemUserName = null;
+            Email = null;
+            Password = null;
+            CreationDateTime = default(DateTime);
+            LastModifiedDateTime = default(DateTime);
+        }
+    }
+}
+
+
+
